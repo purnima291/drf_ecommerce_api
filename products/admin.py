2b200c1f69c71241a16d0ad185385410387e4420
purnima291 <purnimachowrasia@gmail.com>
2025-06-20 12:33:06 +0530

    Register Product and Category model in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'description')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'description')
+    list_filter = ('category', 'created_at', 'price')
+    readonly_fields = ('created_at',)
+    list_select_related = ('category',) # Optimizes database queries
+
